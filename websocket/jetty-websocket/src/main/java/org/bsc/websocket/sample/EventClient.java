1d9b62b1a386aba9b7114d7d73e54fc0f81a8742
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-01-07 19:57:19 +0100

    added jetty websocket module

---
+++ websocket/jetty-websocket/src/main/java/org/bsc/websocket/sample/EventClient.java
@@ -0,0 +1,68 @@
+//
+//  ========================================================================
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.bsc.websocket.sample;
+
+import java.net.URI;
+import java.util.concurrent.Future;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+
+public class EventClient
+{
+    public static void main(String[] args)
+    {
+        URI uri = URI.create("ws://localhost:8080/events/");
+
+        WebSocketClient client = new WebSocketClient();
+        try
+        {
+            try
+            {
+                client.start();
+                // The socket that receives events
+                EventSocketClient socket = new EventSocketClient();
+                // Attempt Connect
+                Future<Session> fut = client.connect(socket,uri);
+                // Wait for Connect
+                Session session = fut.get();
+
+                // Send a message
+                session.getRemote().sendString("Hello");
+
+                // Send another message
+                session.getRemote().sendString("Goodbye");
+
+                // Wait for other size to close
+                socket.awaitClosure();
+
+                // Close session
+                session.close();
+            }
+            finally
+            {
+                client.stop();
+            }
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace(System.err);
+        }
+    }
+}
---
+++ websocket/jetty-websocket/src/main/java/org/bsc/websocket/sample/EventServer.java
@@ -0,0 +1,68 @@
+//
+//  ========================================================================
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.bsc.websocket.sample;
+
+import javax.servlet.ServletException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.server.NativeWebSocketServletContainerInitializer;
+import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter;
+
+public class EventServer
+{
+    public static void main(String[] args) throws ServletException
+    {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        // Setup the basic application "context" for this application at "/"
+        // This is also known as the handler tree (in jetty speak)
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        // Configure specific websocket behavior
+        NativeWebSocketServletContainerInitializer.configure(context,
+            (servletContext, nativeWebSocketConfiguration) ->
+            {
+                // Configure default max size
+                nativeWebSocketConfiguration.getPolicy().setMaxTextMessageBufferSize(65535);
+
+                // Add websockets
+                nativeWebSocketConfiguration.addMapping("/events/*", EventSocketServer.class);
+            });
+
+        // Add generic filter that will accept WebSocket upgrade.
+        WebSocketUpgradeFilter.configure(context);
+
+        try
+        {
+            server.start();
+            server.join();
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace(System.err);
+        }
+    }
+}
