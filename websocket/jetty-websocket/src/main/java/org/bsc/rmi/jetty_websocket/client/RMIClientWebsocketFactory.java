5fc49c2ceb10f8b472ebf5dfed8ababfe46f25ec
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-01-10 19:49:34 +0100

    update

---
+++ websocket/jetty-websocket/src/main/java/org/bsc/rmi/jetty_websocket/client/RMIClientWebsocketFactory.java
@@ -0,0 +1,33 @@
+package org.bsc.rmi.jetty_websocket.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bsc.rmi.jetty_websocket.WebSocket2SocketProxy;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.server.RMIClientSocketFactory;
+
+@Slf4j
+public class RMIClientWebsocketFactory implements RMIClientSocketFactory {
+
+    final int websocket_port;
+
+    public RMIClientWebsocketFactory(int websocket_port) {
+        this.websocket_port = websocket_port;
+    }
+
+    public int getWebsocketPort() {
+        return websocket_port;
+    }
+
+    @Override
+    public Socket createSocket(String host, int rmi_port) throws IOException {
+        try {
+            return new WebSocket2SocketProxy(host, websocket_port, "rmi/pull", rmi_port);
+        } catch (IOException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IOException(e);
+        }
+    }
+}
