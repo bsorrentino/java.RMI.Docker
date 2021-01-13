74b05d28582a899379a79122ede3182b3b9a59f7
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-01-13 20:08:37 +0100

    add custom log formatter

--- samples/src/main/java/org/bsc/rmi/sample/SampleLogFormatter.java
+++ samples/src/main/java/org/bsc/rmi/sample/SampleLogFormatter.java
@@ -1,2 +1,19 @@
-package org.bsc.rmi.sample;public class SampleLogFormatter {
+package org.bsc.rmi.sample;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class SampleLogFormatter extends Formatter {
+    @Override
+    public String format(LogRecord record) {
+        return String.format( "%s %s.%s\n%d - %s\n",
+                record.getLevel(),
+                record.getLoggerName(),
+                record.getSourceMethodName(),
+                //record.getMillis(),
+                record.getThreadID(),
+                record.getMessage()
+                );
+
+    }
 }
