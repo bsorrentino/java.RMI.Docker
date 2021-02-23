e31117aa44866dd565593e892d86ed8540e39762
bsorrentino <bartolomeo.sorrentino@mail.com>
2021-02-23 15:04:29 +0100

    update

--- lipermi/src/main/java/rmi/client/TemperatureMonitor.java
+++ lipermi/src/main/java/rmi/client/TemperatureMonitor.java
@@ -1,24 +1,43 @@
 package rmi.client;
 
 import lombok.extern.slf4j.Slf4j;
+import rmi.SampleRemote;
 import rmi.TemperatureListener;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
 
 
 @Slf4j
 public class TemperatureMonitor extends UnicastRemoteObject implements TemperatureListener
 {
+    private transient SampleRemote remote;
 
-    public TemperatureMonitor() throws RemoteException {}
+    public Optional<SampleRemote> getRemote() {
+        return ofNullable(remote);
+    }
 
-    public TemperatureMonitor( int port ) throws RemoteException {
+    public void setRemote(SampleRemote remote) {
+        this.remote = remote;
+    }
+
+    public TemperatureMonitor(int port ) throws RemoteException {
         super( port );
     }
 
     @Override
     public void temperatureChanged(double temperature) throws RemoteException {
         log.info("Temperature change event {}", temperature);
+
+        getRemote().ifPresent( v -> {
+            try {
+                v.justPass(String.valueOf(temperature));
+            } catch (RemoteException e) {
+                log.warn( "error invoking justPass()", e);
+            }
+        });
     }
 }
